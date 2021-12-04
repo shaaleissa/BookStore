c717a94d7d9df66c15133a43ee2dddef55345260
Fatima117 <93463238+Fatima117@users.noreply.github.com>
2021-12-04 19:47:13 +0300

    Update Studybooks.java

--- Studybooks.java
+++ Studybooks.java
@@ -1,14 +1,14 @@
 
 
 public class Studybooks extends Books {
-    public static int numOfStudybooks;
+    public int numOfStudybooks;
 
     public Studybooks() {
         super();
     }
     public Studybooks(int bookID,String name,int type,double price,int quantity,boolean availability){
     super(bookID,name,type,price,quantity,availability);
-    numOfBooks++;
+    numOfStudybooks++;
 }
     @Override
     public String toString(){
